2f9e768574f44ff640d37547858bc7d867d4de14
Nandan Raj <nraj4741@gmail.com>
2026-08-02 16:09:04 +0530

    fix: RendererBase import error in V2.2

--- tracking/renderers/renderer_base.py
+++ tracking/renderers/renderer_base.py
@@ -1,13 +1,7 @@
 from abc import ABC, abstractmethod
+from tracking.engine.pipeline_context import PipelineContext
 
-class BaseRenderer(ABC):
-    def __init__(self, job_dir: str):
-        self.job_dir = job_dir
-
+class RendererBase(ABC):
     @abstractmethod
-    def render(self):
-        """
-        Reads tracking.json (and overlay configs) to produce an output file 
-        (e.g., .ass, .svg, etc.) in the job directory.
-        """
+    def render(self, context: PipelineContext):
         pass
